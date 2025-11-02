01f3cd15f7f999a8286af4fc7974d1008a9efaf8
Ushing_Marma <ushingmarma72@gmail.com>
2025-11-02 23:27:31 +0600

    __

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\PartnerController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\TrainingScheduleController;
 use App\Http\Controllers\SolutionTrainingController;
+use App\Http\Controllers\IndustryController;
 
 
 Route::get('/user', function (Request $request) {
@@ -24,6 +25,7 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('training-schedules', TrainingScheduleController::class);
     Route::apiResource('solution-trainings', SolutionTrainingController::class);
+    Route::apiResource('industries', IndustryController::class);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/enums/roles', [EnumController::class, 'roles']);
