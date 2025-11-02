d04a3b6026e29cc935a8fdc8da98110e84381561
ishmamazim <ishmamazim2@gmail.com>
2025-11-02 20:46:17 +0600

    crm

--- app/Modules/CRM/Controllers/RecordController.php
+++ app/Modules/CRM/Controllers/RecordController.php
@@ -4,11 +4,48 @@ namespace Modules\CRM\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Modules\CRM\Models\Module;
+use Modules\CRM\Models\Record;
+use Modules\CRM\Models\RecordValue;
 
 class RecordController extends Controller
 {
-    public function index()
+    public function index(Module $module)
     {
-        return response()->json(['message' => 'CRM RecordController works!']);
+        $records = Record::with(['values.field'])
+            ->where('module_id', $module->id)
+            ->get();
+
+        $result = $records->map(function ($rec) {
+            $data = ['id' => $rec->id];
+            foreach ($rec->values as $v) {
+                $data[$v->field->name] = $v->value;
+            }
+            return $data;
+        });
+
+        return response()->json($result);
     }
+
+    public function store(Request $request, Module $module)
+{
+    $fields = $module->fields;
+    $record = Record::create([
+        'module_id' => $module->id,
+        'created_by' => auth()->id(),
+    ]);
+
+    foreach ($fields as $field) {
+        if ($request->has($field->name)) {
+            RecordValue::create([
+                'record_id' => $record->id,
+                'field_id' => $field->id,
+                'value' => $request->input($field->name),
+            ]);
+        }
+    }
+
+    return response()->json($record->load('values.field'));
+}
+
 }
