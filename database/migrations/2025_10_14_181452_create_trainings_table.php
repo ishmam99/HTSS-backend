bd81504c7795dad6a4b263d323400cbad891ffb9
ishmamazim <ishmamazim2@gmail.com>
2025-11-04 00:34:31 +0600

    trainings

--- database/migrations/2025_10_14_181452_create_trainings_table.php
+++ database/migrations/2025_10_14_181452_create_trainings_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('trainings', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('software_id')->nullable()->constrained('softwares')->cascadeOnDelete();
+            $table->foreignId('solution_id')->nullable()->constrained('solutions')->cascadeOnDelete();
+            $table->foreignId('customer_id')->nullable()->constrained('customers')->cascadeOnDelete();
             $table->text('description')->nullable();
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
