0a5fde5006b9e4d732102a24f1df9f8e24df3c43
ishmamazim <ishmamazim2@gmail.com>
2025-11-09 02:05:47 +0600

    industry wise customers

---
+++ app/Http/Resources/CustomerSolutions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerSolutions extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'=>$this->id,
+            'solution_name' => $this->solution?->name,
+            'customer_name' => $this->customer?->load('usre')->name,
+            'softwares' => $this->solution->softwares
+        ];
+    }
+}
