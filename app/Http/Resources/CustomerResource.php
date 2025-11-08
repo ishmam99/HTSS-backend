79c40e7c8108e7a86e183decf14dd5d3913732e4
ishmamazim <ishmamazim2@gmail.com>
2025-11-08 22:23:37 +0600

    customers in industry

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -26,6 +26,8 @@ class CustomerResource extends JsonResource
             'status' => $this->status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'name' => $this->load('user')->name,
+            'name' => $this->load('email')->email,
             'user' => new \App\Http\Resources\UserResource($this->whenLoaded('user')),
         ];
     }
