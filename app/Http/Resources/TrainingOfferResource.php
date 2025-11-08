79568da8b7486b819d4d55bea4a5b5aafcc03bb2
ishmamazim <ishmamazim2@gmail.com>
2025-11-09 02:27:59 +0600

    trainingCourse

--- app/Http/Resources/TrainingOfferResource.php
+++ app/Http/Resources/TrainingOfferResource.php
@@ -17,10 +17,10 @@ class TrainingOfferResource extends JsonResource
             'available_seats' => $this->available_seats,
             'status' => $this->status,
             'trainingEvent' => $this->whenLoaded('event'),
-            'trainingCourse' => $this->whenLoaded('event')->trainingCourse->title,
-            'solution_name' => $this->whenLoaded('event')->trainingCourse->solution->name,
-            'software_name' => $this->whenLoaded('event')->trainingCourse->software->name,
-            'industry_name' => $this->whenLoaded('event')->trainingCourse->industry->name,
+            'trainingCourse' => $this->whenLoaded('event.trainingCourse')->trainingCourse->title,
+            'solution_name' => $this->whenLoaded('event.trainingCourse.solution')->trainingCourse->solution->name,
+            'software_name' => $this->whenLoaded('event.trainingCourse.software')->trainingCourse->software->name,
+            'industry_name' => $this->whenLoaded('event.trainingCourse.industry')->trainingCourse->industry->name,
         ];
     }
 }
