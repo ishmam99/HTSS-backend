7bb9e286a2e06e351b69a4e603ef02cc039d3b42
ishmamazim <ishmamazim2@gmail.com>
2025-11-11 22:46:06 +0600

    end user customer and industry

--- app/Http/Controllers/EndUserController.php
+++ app/Http/Controllers/EndUserController.php
@@ -42,6 +42,11 @@ class EndUserController extends Controller
             ]);
 
             $data = $request->validated();
+            if(auth()->user()->role == 'customer')
+            {
+                $data['customer_id'] = auth()->user()->customer->id;
+                $data['industry_id'] = auth()->user()->customer->industry_id;
+            }
             $data['user_id'] = $user->id;
 
             // Create EndUser
