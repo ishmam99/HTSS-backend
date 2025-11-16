73598b918097f885e6cfd48a9c7af603d9ca17d9
ishmamazim <ishmamazim2@gmail.com>
2025-11-16 20:08:32 +0600

    proposal

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use App\Http\Controllers\EndUserSoftwareController;
 use App\Http\Controllers\IndustryController;
 use App\Http\Controllers\IssueTicketController;
 use App\Http\Controllers\OnsiteSupportTicketController;
-use App\Http\Controllers\ProposalController;
 use App\Http\Controllers\TrainerController;
 use App\Http\Controllers\TrainingCourseController;
 use App\Http\Controllers\TrainingEnrollmentController;
@@ -79,8 +78,7 @@ Route::prefix('v1')->group(function () {
     Route::get('end-user-solution-list',[EndUserSoftwareController::class, 'getSolutions']);
     Route::post('end-user-solution-add',[EndUserSoftwareController::class, 'addSolution']);
 
-     Route::apiResource('proposal',ProposalController::class);
-     Route::put('status-update-proposal/{dealId}/{proposalId}', [ProposalController::class, 'updateStatus']);
+
     });
 
 });
