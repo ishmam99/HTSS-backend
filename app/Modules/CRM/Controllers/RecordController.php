0407740a8135c6822409a8a285c0818a9c1d6a54
ishmamazim <ishmamazim2@gmail.com>
2025-11-17 01:26:45 +0600

    show record

--- app/Modules/CRM/Controllers/RecordController.php
+++ app/Modules/CRM/Controllers/RecordController.php
@@ -33,7 +33,7 @@ class RecordController extends Controller
 public function index(Module $module)
 {
     $query = $module->records()
-        ->with(['records.values.field', 'records.assignments.user']);
+        ->with(['values.field', 'assignments.user']);
 
     if (request()->date_field && request()->start_date && request()->end_date) {
 
