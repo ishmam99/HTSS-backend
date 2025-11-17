2ed15da49116358644ce67604590a3c8305b65ea
ishmamazim <ishmamazim2@gmail.com>
2025-11-17 21:27:26 +0600

    order by module fields

--- app/Modules/CRM/Controllers/RecordController.php
+++ app/Modules/CRM/Controllers/RecordController.php
@@ -54,7 +54,7 @@ public function index(Module $module)
 
         $query->whereHas('values', function ($q) use ($fieldName, $fieldValue) {
             $q->whereHas('field', fn($f) => $f->where('name', $fieldName))
-                ->where('value', $fieldValue)->orderBy('order', 'asc');
+                ->where('value', $fieldValue);
         });
     }
 
