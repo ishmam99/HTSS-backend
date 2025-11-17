558172cffae1291a726f864b26c51a5fbc959b00
ishmamazim <ishmamazim2@gmail.com>
2025-11-17 21:20:22 +0600

    order by module fields

--- app/Modules/CRM/Models/Record.php
+++ app/Modules/CRM/Models/Record.php
@@ -9,7 +9,11 @@ class Record extends Model
     protected $guarded = [];
      public function values()
     {
-        return $this->hasMany(RecordValue::class,'record_id');
+        return $this->hasMany(RecordValue::class,'record_id')
+         ->with('field')
+        ->join('module_fields', 'record_values.field_id', '=', 'module_fields.id')
+        ->orderBy('module_fields.order', 'asc')
+        ->select('record_values.*');
     }
 
     public function module()
