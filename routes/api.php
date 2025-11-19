1976e7bbe4095220a3ff5b86b7b8144f97c60337
Ushing_Marma <ushingmarma72@gmail.com>
2025-11-19 21:20:58 +0600

    __

--- routes/api.php
+++ routes/api.php
@@ -28,25 +28,6 @@ Route::prefix('v1')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
-<<<<<<< HEAD
-        Route::get('/enums/roles', [EnumController::class, 'roles']);
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::apiResource('partners', PartnerController::class);
-        Route::apiResource('customers', CustomerController::class);
-        Route::apiResource('training-schedules', TrainingScheduleController::class);
-        Route::apiResource('solution-trainings', SolutionTrainingController::class);
-
-        Route::post('add-industry-solutions', [SoftwareController::class, 'industrySolution']);
-        Route::post('add-industry-softwares', [SoftwareController::class, 'industrySoftware']);
-        Route::post('add-software-solutions', [SoftwareController::class, 'softwareSolution']);
-        Route::apiResource('trainings', TrainingController::class);
-        Route::apiResource('industries', IndustryController::class);
-        Route::apiResource('software-skills', SoftwareSkillController::class);
-        Route::apiResource('solutions', SolutionController::class);
-        Route::apiResource('softwares', SoftwareController::class);
-        Route::apiResource('training-sessions', TrainingSessionController::class);
-        Route::apiResource('training-enrollments', TrainingEnrollmentController::class);
-=======
     Route::get('/enums/roles', [EnumController::class, 'roles']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('partners', PartnerController::class);
@@ -59,8 +40,7 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('software-skills', SoftwareSkillController::class);
     Route::apiResource('solutions', SolutionController::class);
     Route::apiResource('softwares', SoftwareController::class);
-       
->>>>>>> b05dacffebeae60522b7fcb9454bc8882abb4c29
+
         Route::prefix('users/{userId}')->group(function () {
             Route::get('software-skills', [UserSoftwareSkillController::class, 'index']);
             Route::post('software-skills', [UserSoftwareSkillController::class, 'store']);
@@ -68,8 +48,6 @@ Route::prefix('v1')->group(function () {
             Route::delete('software-skills/{softwareSkillId}', [UserSoftwareSkillController::class, 'destroy']);
         });
     });
-<<<<<<< HEAD
-=======
     Route::apiResource('end-users', EndUserController::class);
     Route::apiResource('training-course', TrainingCourseController::class);
     Route::apiResource('trainer', TrainerController::class);
@@ -80,5 +58,4 @@ Route::prefix('v1')->group(function () {
 
 
 
->>>>>>> b05dacffebeae60522b7fcb9454bc8882abb4c29
 });
