1dc5a6ef827a8c7d6b4f2f053e5c4f72821ccf2a
dipto <diptoguho4@gmail.com>
2025-11-20 20:33:44 +0600

    end user

--- app/Http/Controllers/EndUserTrainingController.php
+++ app/Http/Controllers/EndUserTrainingController.php
@@ -26,11 +26,11 @@ class EndUserTrainingController extends Controller
 
     public function index(Request $request)
     {
-        $query = EndUserTraining::with(['user','offer'])->when($request->end_user_id, function ($query, $request) {
-            return $query->where('end_user_id', $request->end_user_id);
-        })->when($request->training_offer_id, function ($query, $request) {
-            return $query->where('training_offer_id', $request->training_offer_id);
-        })->orderBy('id', 'desc');
+       $query = EndUserTraining::with(['user','offer'])
+    ->when($request->end_user_id, fn($q) => $q->where('end_user_id', $request->end_user_id))
+    ->when($request->training_offer_id, fn($q) => $q->where('training_offer_id', $request->training_offer_id))
+    ->orderBy('id', 'desc');
+
 
            $lists = $request->has('per_page')
         ? $query->paginate($request->per_page)
