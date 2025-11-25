316b75b8348d7c56df3d004f9682c12de3e16c9b
dipto <diptoguho4@gmail.com>
2025-11-25 21:40:25 +0600

    attendance

---
+++ app/Http/Resources/AttendanceTimeResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AttendanceTimeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'record_id' => $this->record_id,
+            'type_of_work' => $this->type_of_work,
+            'notes' => $this->notes,
+            'total_minute' => $this->total_minute,
+            'status' => $this->status,
+            'attachment' => $this->attachment 
+                ? asset('storage/' . $this->attachment) 
+                : null,
+        ];
+    }
+}
