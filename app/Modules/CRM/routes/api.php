0d4b38d4cda03f529f8d6ec025abd941f116cf00
ishmamazim <ishmamazim2@gmail.com>
2025-11-30 02:35:47 +0600

    log check

--- app/Modules/CRM/routes/api.php
+++ app/Modules/CRM/routes/api.php
@@ -35,7 +35,7 @@ Route::prefix('api/v1')->middleware('api')->group(function () {
         Route::delete('/record/{record}', [RecordController::class, 'destroy']);
         Route::get('convert-deal-to-project/{dealId}',[RecordController::class,'convertDealToProject']);
         Route::post('bulk-update-records',[RecordValueController::class,'bulkUpdateOrCreate']);
-        Route::post('custom-views',[CustomViewController::class,'sto    re']);
+        Route::post('custom-views',[CustomViewController::class,'store']);
         Route::get('my-custom-views',[CustomViewController::class,'index']);
         Route::get('my-custom-views/{id}',[CustomViewController::class,'show']);
     });
