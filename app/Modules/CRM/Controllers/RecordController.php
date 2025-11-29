e4604e3ebc899952c519354888ee0f500ed80ac2
ishmamazim <ishmamazim2@gmail.com>
2025-11-30 02:04:59 +0600

    log check

--- app/Modules/CRM/Controllers/RecordController.php
+++ app/Modules/CRM/Controllers/RecordController.php
@@ -32,7 +32,7 @@ public function index(Module $module)
     // Apply custom view filter
     $viewId = request()->custom_view_id;
   $view = CustomView::with('rootGroup.childrenRecursive.conditions')->find($viewId);
-
+        \Log::info($view);
     if ($view) {
         \Log::info($view);
         $query = $this->applyCustomViewFilter($query, $view);
