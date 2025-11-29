4c313c29c2c2cd893cf175ec73bc9ae8efe60697
ishmamazim <ishmamazim2@gmail.com>
2025-11-30 01:50:59 +0600

    log check

--- app/Modules/CRM/Controllers/CustomViewController.php
+++ app/Modules/CRM/Controllers/CustomViewController.php
@@ -47,7 +47,7 @@ private function saveGroup($customViewId, $parentId, $groupData)
         'join_type' => $groupData['join_type'] ?? 'AND',
         'order' => $groupData['order'] ?? 0
     ]);
-
+    \Log::info($group);
     // Save conditions
     if (!empty($groupData['conditions']) && $group) {
         foreach ($groupData['conditions'] as $index => $cond) {
