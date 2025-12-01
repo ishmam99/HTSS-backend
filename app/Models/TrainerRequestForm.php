e832ef7ba7152ca61dd615cf02cf7a578c0f7a4f
dipto <diptoguho4@gmail.com>
2025-12-02 01:27:47 +0600

    request form

--- app/Models/TrainerRequestForm.php
+++ app/Models/TrainerRequestForm.php
@@ -10,4 +10,19 @@ class TrainerRequestForm extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function industry()
+    {
+        return $this->belongsTo(Industry::class);
+    }
+
+    public function solution()
+    {
+        return $this->belongsTo(Solution::class);
+    }
+
+    public function software()
+    {
+        return $this->belongsTo(Software::class);
+    }
 }
