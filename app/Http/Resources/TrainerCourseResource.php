f0334cafb9acd5c552ca8456f0f9ca86d1aa034b
ishmamazim <ishmamazim2@gmail.com>
2025-12-03 21:04:12 +0600

    training course

--- app/Http/Resources/TrainerCourseResource.php
+++ app/Http/Resources/TrainerCourseResource.php
@@ -10,7 +10,7 @@ class TrainerCourseResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'training_course' => $this->trainingCourse->with('software,solution,industry'),
+            'training_course' => $this->trainingCourse->load('software,solution,industry'),
             'trainer_id' => $this->trainer,
             'status' => $this->status,
         ];
