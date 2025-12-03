78b837a5f46a033d60f7555b6f3d89c34bd969f0
dipto <diptoguho4@gmail.com>
2025-12-03 19:34:51 +0600

    training module

--- app/Http/Resources/TrainerCourseResource.php
+++ app/Http/Resources/TrainerCourseResource.php
@@ -10,7 +10,7 @@ class TrainerCourseResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'training_course' => $this->training_course,
+            'training_course' => $this->trainingCourse,
             'trainer_id' => $this->trainer,
             'status' => $this->status,
         ];
