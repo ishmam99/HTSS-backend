ca86d09088c83c4f04a10809ed16dade7e671997
ishmamazim <ishmamazim2@gmail.com>
2025-12-03 21:01:19 +0600

    Merge branch 'main' of http://gitlab.htbbd.com/websites/hi-tech-softsys-backend-service

--- app/Http/Resources/TrainerCourseResource.php
+++ app/Http/Resources/TrainerCourseResource.php
@@ -10,7 +10,7 @@ class TrainerCourseResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'training_course' => $this->training_course->load('software,solution,industry'),
+            'training_course' => $this->training_course->with('software,solution,industry'),
             'trainer_id' => $this->trainer,
             'status' => $this->status,
         ];
