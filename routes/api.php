e046fda9a4bd8093eb570fd676f5395f3e01738f
Ushing_Marma <ushingmarma72@gmail.com>
2026-01-13 20:52:24 +0600

    customer-success-managers/{csm}/customers

--- routes/api.php
+++ routes/api.php
@@ -138,7 +138,6 @@ Route::prefix('v1')->group(function () {
 
     Route::get('/customers/by-user/{user}', [CustomerController::class, 'getByUser']);
 
-    Route::get(
-    '/customer-success-managers/{csm}/customers',[CustomerSuccessManagerController::class, 'customersByCsm']);
+    Route::get('/customer-success-managers/{csm}',[CustomerSuccessManagerController::class, 'customersByCsm']);
 
 });
