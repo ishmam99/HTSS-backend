ba9bdbcc15c027265d7b31a94a737c9aa9febecb
Ushing_Marma <ushingmarma72@gmail.com>
2026-01-13 21:47:48 +0600

    ____

--- app/Http/Controllers/CustomerSuccessManagerController.php
+++ app/Http/Controllers/CustomerSuccessManagerController.php
@@ -180,7 +180,7 @@ class CustomerSuccessManagerController extends Controller
             $q->where('user_id', $csm->user_id);
         })
             ->with('user:id,name')
-            ->select('id', 'user_id')
+            ->select('customers.id', 'customers.user_id')
             ->get()
             ->map(function ($customer) {
                 return [
