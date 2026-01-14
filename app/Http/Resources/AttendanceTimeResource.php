4c386dea807b47afe4beead4e10fd9cfab56a3f1
Piayl Guho Dipto <piyal.g.dipto@htbbd.com>
2026-01-15 00:28:12 +0600

    status

--- app/Http/Resources/AttendanceTimeResource.php
+++ app/Http/Resources/AttendanceTimeResource.php
@@ -20,6 +20,7 @@ class AttendanceTimeResource extends JsonResource
             'type_of_work' => $this->type_of_work,
             'notes' => $this->notes,
             'total_minute' => $this->total_minute,
+            'activity' => $this->activity,
             'total_hours' => number_format($this->total_minute/60, 2),
             'status' => $this->status,
             'attachment' => $this->attachment
