5ae3855abcbe15782ebadb806fce77175bced414
ishmamazim <ishmamazim2@gmail.com>
2026-01-21 23:43:36 +0600

    module with fields count

--- app/Modules/CRM/Controllers/ModuleController.php
+++ app/Modules/CRM/Controllers/ModuleController.php
@@ -13,7 +13,7 @@ class ModuleController extends Controller
 {
    public function index()
    {
-     $modules = Module::all();
+     $modules = Module::with('fields.count')->get();
      return response()->json($modules);
    }
 public function stats(Request $request)
