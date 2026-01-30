4bbaaca67cc7b4171a778bc7cc2c712f5606444a
ishmamazim <ishmamazim2@gmail.com>
2026-01-31 03:23:18 +0600

    customer with industry

--- app/Http/Controllers/SuccessTeamController.php
+++ app/Http/Controllers/SuccessTeamController.php
@@ -58,7 +58,7 @@ class SuccessTeamController extends Controller
     // Show single team
     public function show($id)
     {
-        $team = SuccessTeam::with(['members', 'companies.customers.user', 'owner'])->findOrFail($id);
+        $team = SuccessTeam::with(['members', 'companies.customers.user','companies.customers.industry', 'owner'])->findOrFail($id);
         return response()->json($team);
     }
 
