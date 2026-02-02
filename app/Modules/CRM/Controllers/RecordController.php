3249283cd9eb09861731750e8e43f72ff2f8b3b9
ishmamazim <ishmamazim2@gmail.com>
2026-02-02 21:46:54 +0600

    company wise deal and contacts get

--- app/Modules/CRM/Controllers/RecordController.php
+++ app/Modules/CRM/Controllers/RecordController.php
@@ -476,6 +476,7 @@ public function convertModule($recordId)
         if(request()->has('company_id'))
             {
                 $recordIds = Customer::where('company_id',request()->company_id)->pluck('record_id');
+                dd($recordIds);
                   $childIds = RecordRelation::whereIn('parent_record_id', $recordIds)
         ->where('relation_type', $type)
         ->pluck('child_record_id');
