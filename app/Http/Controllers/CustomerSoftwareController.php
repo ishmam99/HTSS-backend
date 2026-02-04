ea6c089b951f417102628e99539a361894183a14
ishmamazim <ishmamazim2@gmail.com>
2026-02-05 02:53:03 +0600

    customer softwares

--- app/Http/Controllers/CustomerSoftwareController.php
+++ app/Http/Controllers/CustomerSoftwareController.php
@@ -11,17 +11,17 @@ class CustomerSoftwareController extends Controller
     public function index(Request $request)
     {
         $query = CustomerSoftware::with(['customer.user', 'software'])
-            ->when(auth()->user()->role === 'customer', function ($q) {
+            ->when(auth()->check() && auth()->user()->role === 'customer', function ($q) {
                 $q->where('customer_id', auth()->user()->customer->id);
             })
             ->when($request->filled('customer_id'), function ($q) use ($request) {
                 $q->where('customer_id', $request->customer_id);
             })
             ->when($request->filled('customer_ids'), function ($q) use ($request) {
-                $q->whereIn('customer_ids', $request->customer_ids);
+                $q->whereIn('customer_id', $request->customer_ids);
             })
             ->when($request->filled('software_ids'), function ($q) use ($request) {
-                $q->whereIn('software_ids', $request->software_ids);
+                $q->whereIn('software_id', $request->software_ids);
             })
             ->when($request->filled('software_id'), function ($q) use ($request) {
                 $q->where('software_id', $request->software_id);
--- app/Http/Controllers/CustomerSolutionController.php
+++ app/Http/Controllers/CustomerSolutionController.php
@@ -20,13 +20,13 @@ class CustomerSolutionController extends Controller
             $q->where('customer_id', $request->customer_id);
         })
         ->when($request->filled('customer_ids'), function ($q) use ($request) {
-            $q->whereIn('customer_ids', $request->customer_ids);
+            $q->whereIn('customer_id', $request->customer_ids);
         })
         ->when($request->filled('solution_id'), function ($q) use ($request) {
             $q->where('solution_id', $request->solution_id);
         })
         ->when($request->filled('solution_ids'), function ($q) use ($request) {
-            $q->whereIn('solution_ids', $request->solution_ids);
+            $q->whereIn('solution_id', $request->solution_ids);
         })
         ->when($request->filled('softwares'), function ($q) {
             $q->with('solution.softwares');
