a422850358f5094abc6ce106bef3e88c244c7ac2
ishmamazim <ishmamazim2@gmail.com>
2026-06-07 01:56:06 +0600

    course ids

--- app/Models/TrainerPreferdSchedule.php
+++ app/Models/TrainerPreferdSchedule.php
@@ -8,4 +8,13 @@ class TrainerPreferdSchedule extends Model
 {
     //
      protected $guarded = ['id'];
+
+     public function trainerRequestForm()
+     {
+         return $this->belongsTo(TrainerRequestForm::class, 'trainer_request_form_id');
+     }
+     public function trainer()
+     {
+         return $this->belongsTo(User::class, 'trainer_id');
+     }
 }
