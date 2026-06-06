4fdd8fdb7c9044638131f59a4a391f245e140f16
ishmamazim <ishmamazim2@gmail.com>
2026-06-06 21:58:01 +0600

    trainer courses

--- database/migrations/2025_12_02_153608_create_trainer_courses_table.php
+++ database/migrations/2025_12_02_153608_create_trainer_courses_table.php
@@ -10,7 +10,8 @@ return new class extends Migration
     {
         Schema::create('trainer_courses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('trainer_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('trainer_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->foreignId('trainer_request_form_id')->nullable()->constrained('trainer_request_forms')->cascadeOnDelete();
             $table->foreignId('training_course_id')->constrained('training_courses')->cascadeOnDelete();
             $table->tinyInteger('status')->default(0);
             $table->timestamps();
